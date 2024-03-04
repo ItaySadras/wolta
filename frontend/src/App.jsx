dfbc5d314475d0ea29ddbeb2053cfc585860565e
Itay Sadras <itaysadr@gmail.com>
2024-03-04 17:31:34 +0200

    created login and register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 function App() {
 
   return (
     <>
-     
+     <Register/>
     </>
   );
 }

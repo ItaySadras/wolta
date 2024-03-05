232590797dacf128862ca5cf92e3e3275911edd6
Itay Sadras <itaysadr@gmail.com>
2024-03-05 16:59:26 +0200

    started working on restaurant backend

---
+++ backend/roots/controllers/restaurantController.js
@@ -0,0 +1,23 @@
+const Restaurant = require("../models/restaurantModel")
+
+exports.createRestaurant = async (req, res) => {
+    try {
+        const { userName, password, email, phoneNumber } = req.body;
+        const newRestaurant = new Restaurant({
+            userName,
+            password,
+            email,
+            phoneNumber
+        })
+        await newRestaurant.save();
+        res
+            .status(201)
+            .json({ message: "Restaurant created successfully", restaurant: newRestaurant });
+
+    } catch (error) {
+        res.status(404).json({
+            status: failed,
+            message: error.message
+        })
+    }
+}

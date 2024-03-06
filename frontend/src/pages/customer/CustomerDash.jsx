c23e7d2e3785d6db578e22a5dac6f8b03aaccb45
Itay Sadras <itaysadr@gmail.com>
2024-03-06 15:48:08 +0200

    built customer dashboard lazy load

--- frontend/src/pages/customer/CustomerDash.jsx
+++ frontend/src/pages/customer/CustomerDash.jsx
@@ -1,30 +1,29 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CustomerSearch from '../../components/customerDash/CustomerSearch';
-// Assuming RestaurantContext is not used in this snippet
+import { RestaurantContext } from '../../context/RestaurantContext';
 
 const CustomerDash = () => {
- const restaurants = [
-    {
-      restaurantName: "The Great Pizza",
-      address: "123 Pizza Street, Pizza City, PZ",
-      restaurantFilter: "Italian",
-      open: true,
-    },
-    {
-      restaurantName: "Sushi Paradise",
-      address: "456 Sushi Avenue, Sushi Town, SU",
-      restaurantFilter: "Japanese",
-      open: false
-    },
-    {
-      restaurantName: "Burger Bistro",
-      address: "789 Burger Boulevard, Burgerville, BU",
-      restaurantFilter: "Fast Food",
-      open: true
-    }
- ];
+  const { restaurants, getAllRestaurants } = useContext(RestaurantContext);
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(4);
 
- return (
+  const handleLoadMore = () => {
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    if (page > 1) {
+      getAllRestaurants(page, limit);
+    }
+  }, [page])
+
+  const generalAddress = {
+    streetname: "hahilazon",
+    streetNumber: "3",
+    city: "ramat gan",
+  }
+
+  return (
     <div>
       <div>
         <h2>Your location: {/* user location */}</h2>
@@ -36,16 +35,33 @@ const CustomerDash = () => {
         <ol>
           {restaurants.map((restaurant, index) => (
             <li key={index}>
-              {restaurant.restaurantName}
-              {restaurant.address}
-              {restaurant.restaurantFilter}
-              {restaurant.open ? 'Open' : 'Closed'}
+              <p>{restaurant.restaurantName}</p>
+              <p>
+                address:
+                <ul>
+                  {Object.values(generalAddress).map((value, index) => (
+                    <li key={index}>{value}</li>
+                  ))}
+                </ul>
+              </p>
+              <p>
+                filters:
+                <ul>
+                  {restaurant.restaurantFilter.map((element, index) => (
+                    <li key={index}>{element}</li>
+                  ))}
+                </ul>
+              </p>
+              <p>{restaurant.open ? 'Open' : 'Closed'}</p>
             </li>
           ))}
         </ol>
       </div>
+      <div>
+        <button onClick={handleLoadMore}>Load More</button>
+      </div>
     </div>
- );
+  );
 }
 
 export default CustomerDash;

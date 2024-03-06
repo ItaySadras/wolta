0486cc38d2cf63f026fd24426c86a60784435d2a
Noam Travis <noamtravis@Noams-MacBook-Pro.local>
2024-03-06 16:28:58 +0200

    workingoncrudsss

--- backend/app.js
+++ backend/app.js
@@ -3,19 +3,16 @@ const express = require("express"); // Express.js web application framework
 
 const app = express();
 app.use(express.json());
-app.use(cors( ));
+app.use(cors());
 
-const customerRoutes = require('./roots/routes/customerRoutes')
+const customerRoutes = require("./roots/routes/customerRoutes");
 const courierRoutes = require("./roots/routes/courierRoute");
 const restaurantRoute = require("./roots/routes/restaurantRoute");
 
-
-app.use('/api/customer', customerRoutes)
-app.use('/api/courier', courierRoutes);
-app.use('/api/restaurant', restaurantRoute);
-
-
-
+app.use("/api/customer", customerRoutes);
+app.use("/api/courier", courierRoutes);
+app.use("/api/restaurant", restaurantRoute);
+app.use("/api/review", reviewRoute);
 
 // Export the app to be used by the server
 module.exports = app;

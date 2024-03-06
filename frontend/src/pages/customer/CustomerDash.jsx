98dafbfc387b9ff861286e032230c992c18e1aae
Itay Sadras <itaysadr@gmail.com>
2024-03-06 13:38:25 +0200

    worked on customer routes and context

--- frontend/src/pages/customer/CustomerDash.jsx
+++ frontend/src/pages/customer/CustomerDash.jsx
@@ -2,6 +2,45 @@ import React from 'react'
 import CustomerSearch from '../../components/customerDash/CustomerSearch'
 
 const CustomerDash = () => {
+  const restaurants = [
+    {
+      restaurantName: "Le Bistro",
+      lcoation: "hahilazon 3, ramat gan",
+      cuisine: "italian",
+
+    },
+    {
+      restaurantName: "Burger Barn",
+      lcoation: "mapu 19, raanana",
+      cuisine: "hamburgers",
+
+    },
+    {
+      restaurantName: "Olive Garden",
+      lcoation: "batei rishonim 2, merhavia",
+      cuisine: "italian",
+
+    },
+    {
+      restaurantName: "Le Bistro",
+      lcoation: "hahilazon 3, ramat gan",
+      cuisine: "italian",
+
+    },
+    {
+      restaurantName: "Le Bistro",
+      lcoation: "hahilazon 3, ramat gan",
+      cuisine: "italian",
+
+    },
+    {
+      restaurantName: "Le Bistro",
+      lcoation: "hahilazon 3, ramat gan",
+      cuisine: "italian",
+
+    },
+
+  ]
   return (
     <div>
       <div>
@@ -11,10 +50,16 @@ const CustomerDash = () => {
         <CustomerSearch />
       </div>
       <div>
-        {/* restaurant recommendations */}
+        <ol>
+          {restaurants.map((restaurant, index) => {
+            <li key={index}>
+              {restaurant.restaurantName}
+            </li>
+          })}
+        </ol>
       </div>
     </div>
   )
 }
 
-export default CustomerDash
+export default CustomerDash

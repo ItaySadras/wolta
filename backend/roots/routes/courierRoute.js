5c1a797ed0bd15c0a89321afa3ad87ed96702726
Ron <avitalron12@gmail.com>
2024-03-07 14:52:00 +0200

    GeoLocation funcunality

--- backend/roots/routes/courierRoute.js
+++ backend/roots/routes/courierRoute.js
@@ -6,6 +6,7 @@ router.post('/', courierController.createCourier);
 // router.patch('/vehicle', courierController.setVehicle);
 router.patch('/rating/:id', courierController.setRating);
 router.patch('/available/:id', courierController.setAvailable);
+router.patch('currOrder/:id', courierController.setCurrOrder);
 
 
 module.exports = router;

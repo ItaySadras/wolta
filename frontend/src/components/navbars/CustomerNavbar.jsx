726c8d259f49faa49f737f01269162b4722bbad9
roeeefron <140965866+roeeke@users.noreply.github.com>
2024-03-07 12:40:34 +0200

    design of navbar1

--- frontend/src/components/navbars/CustomerNavbar.jsx
+++ frontend/src/components/navbars/CustomerNavbar.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './navbar.css'; // Make sure to import the CSS file correctly
 
 const CustomerNavbar = () => {
   return (
-    <div>
-      <div>
-        {/* logo */}
-        <div>
-          BaliFood
+    <nav className="customer-navbar">
+      <div className="logo-container">
+        <img src="" alt="BaliFood Logo" className="navbar-logo" />
+        <div className="brand-name">
+          <h1>BaliFood</h1>
         </div>
       </div>
-      <div>
+      <div className="profile-section">
         profile
       </div>
-      <div>
+      <div className="greeting-section">
         <h1>Hi! {/* name of user */}</h1>
       </div>
-      <div>
-        logout button
+      <div className="logout-section">
+        <button className="logout-button">Logout</button>
       </div>
-    </div>
-  )
+    </nav>
+  );
 }
 
-export default CustomerNavbar
+export default CustomerNavbar;

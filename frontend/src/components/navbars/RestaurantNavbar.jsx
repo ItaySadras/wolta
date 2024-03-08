4de451b86b4b2fdb0574514f2d4c39dbb6ac88cf
Noam Travis <noamtravis@Noams-MacBook-Pro.local>
2024-03-08 11:56:38 +0200

    finishnavbars

--- frontend/src/components/navbars/RestaurantNavbar.jsx
+++ frontend/src/components/navbars/RestaurantNavbar.jsx
@@ -1,31 +1,38 @@
-import React from 'react'
+import React from "react";
+import "./navbar.css";
+import logoImage from "../../assets/BaliFoodLogoNOBG.png";
+import { NavLink } from "react-router-dom";
 
 const RestaurantNavbar = () => {
   return (
-    <div>
-      <div>
-        {/* logo */}
-        <div>
-          BaliFood
-        </div>
+    <nav className="navbar">
+      <div className="logo-container">
+        <NavLink to={"/restaurant/:restaurantId/profile"}>
+          <img src={logoImage} alt="BaliFood Logo" className="navbar-logo" />
+        </NavLink>
       </div>
-      <div>
-        menu
+      <div className="nav-section">
+        <li>
+          <NavLink to={"/restaurant/:restaurantId/menu"}>
+            <button className="navbutton">Menu</button>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/restaurant/:restaurantId/reviews"}>
+            <button className="navbutton">Reviews</button>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/restaurant/:restaurantId/restaurantPage"}>
+            <button className="navbutton">Profile</button>
+          </NavLink>
+        </li>
+        <li>
+          <button className="logout-button">Logout</button>
+        </li>
       </div>
-      <div>
-        reviews
-      </div>
-      <div>
-        profile
-      </div>
-      <div>
-        <h1>Hi! {/* name of user */}</h1>
-      </div>
-      <div>
-        logout button
-      </div>
-    </div>
-  )
-}
+    </nav>
+  );
+};
 
-export default RestaurantNavbar
+export default RestaurantNavbar;

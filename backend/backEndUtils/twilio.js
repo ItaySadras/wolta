b30b435fea40405954dc8b822e7fd0c66b044cc2
reambiger <you@example.com>
2024-03-09 21:42:00 +0200

    filterDone

--- backend/backEndUtils/twilio.js
+++ backend/backEndUtils/twilio.js
@@ -4,7 +4,7 @@ dotenv.config({ path: "../.env" });
 const { Body } = require("twilio/lib/twiml/MessagingResponse");
 const accountSid = process.env.TWILLIO_AUTH_SID;
 const authToken = process.env.TWILLIO_AUTH_TOKEN;
-console.log("🚀 ~ authToken:", authToken);
+const myNumber = process.env.TWILLIO_MY_NUMBER;
 
 // client.messages
 //   .create({
@@ -16,20 +16,17 @@ console.log("🚀 ~ authToken:", authToken);
 
 const client = require("twilio")(accountSid, authToken);
 const sendSMS = async (username,phoneNumber) => {
-    console.log("🚀 ~ sendSMS ~ phoneNumber:", phoneNumber)
     const website_url="http://localhost:5173/"
   client.messages
     .create({
       body: ` ${username}! would you spend one minuets of yourTime to help as be better and feal this quick Survey : ${website_url}`,
-      from: "+13343199573",
+      from: myNumber,
       to: phoneNumber,
     })
     .then((message) => console.log(message.sid));
 };
 
-// const accountSid = 'AC98099cc24cf48886b49209c199cf0fd8';
-// const authToken = '[AuthToken]';
 
-sendSMS("ream","+972546663286");
+sendSMS();
 
 module.exports = { sendSMS };

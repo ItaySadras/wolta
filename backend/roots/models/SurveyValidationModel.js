6e75e9779986dd69861d8b8f9cc2acf9269c09cd
reambiger <you@example.com>
2024-03-10 12:02:47 +0200

    reviewCookies2

--- backend/roots/models/SurveyValidationModel.js
+++ backend/roots/models/SurveyValidationModel.js
@@ -5,7 +5,7 @@ const surveyValidationSchema = new mongoose.Schema({
  crypt:{type:String}
 
 });
-
+// hey
 const SurveyValidation = mongoose.model("SurveyValidation", surveyValidationSchema);
 
 module.exports = SurveyValidation;

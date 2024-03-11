2c674985477a9ae76e8606bdfc9368491d89a273
roeeefron <140965866+roeeke@users.noreply.github.com>
2024-03-11 11:12:34 +0200

    roeefrontcourr

--- backend/roots/models/orderModel.js
+++ backend/roots/models/orderModel.js
@@ -4,7 +4,7 @@ const orderSchema = new mongoose.Schema({
     customer:{type: mongoose.Schema.Types.ObjectId, ref: "Customer"},
     courier:{type: mongoose.Schema.Types.ObjectId, ref: "Courier"},
     restaurant:{type: mongoose.Schema.Types.ObjectId, ref: "Restaurant"},
-    orderDishes:{dish:{type: mongoose.Schema.Types.ObjectId, ref: "Dish"},comment:{type:String}},
+    orderDishes:[{dish:{type: mongoose.Schema.Types.ObjectId, ref: "Dish"},comment:{type:String}}],
     arrivingTime:{type:String}
 
     
--- backend/roots/routes/customerRoutes.js
+++ backend/roots/routes/customerRoutes.js
@@ -25,4 +25,6 @@ router.put('/updateDetails/:customerId', customerController.UpdateCustomerDetail
 
 router.route('/creteOrder/:').post(orderController.createOrder)
 
+
+
 module.exports = router;
